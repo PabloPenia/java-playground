2d54c68ef8aae52edd1e30f4f76b912505ca0d5e
Pablo Peña <pablopenia.dev@gmail.com>
2023-03-21 15:51:44 -0300

    testing

---
+++ test/src/model/Pet.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Pet {
+	private String name;
+	private LocalDate dob;
+	
+	public Pet() {
+	}
+	
+	public Pet(String name, LocalDate dob) {
+		this.name = name;
+		this.dob = dob;
+	}
+	
+	
+
+}
